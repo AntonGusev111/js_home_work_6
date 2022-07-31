3433e56cd562aa5b846ab4124c68d24bd0626f4a
GusevADresume <resumeGusevAD@gmail.com>
2022-07-31 22:54:52 +0300

    f1

--- task1/task.js
+++ task1/task.js
@@ -3,15 +3,14 @@ function workWithModal () {
     const modalClose = document.getElementById("modal_close");
     const modalButton = document.getElementById("modal_button");
     const modalSuccess = document.getElementById("modal_success");
+    const close = document.getElementById("modal_close1")
     modalClose.onclick = () => {modal.className = 'modal';}
     modalButton.onclick = () => {
         modalSuccess.className = "modal modal_active";
-        let close = document.getElementById("modal_close1")
+        modal.className = 'modal';
         close.onclick = () => {
             modal.className = 'modal';
             modalSuccess.className= "modal";}
-        //event.target.style.backgroundColor = 'green';
-        //modalButton.innerText = 'Хорошо сделано!';
     }
 }
 
--- task3/task.js
+++ task3/task.js
@@ -6,17 +6,24 @@ function clickFound() {
 }
 
 function clickDefine (object) {
-    let pictIndex = 0
-    const pictures = Array.from(document.getElementsByClassName("slider__items"))
-    let pict = pictures[0].children[pictIndex]
-    if (object == 'slider__arrow slider__arrow_prev'){ console.log('prev')
-    
-    console.log(pict).className.includes ('slider__item')
+    const pictures = Array.from(document.getElementsByClassName("slider__items")[0].children)
+    let pictIndex = pictures.findIndex(callback)
+    pictures[pictIndex].className='slider__item'
+    if (object == 'slider__arrow slider__arrow_prev'){ 
+        pictIndex = pictIndex > 0 ? pictIndex -= 1 : pictIndex = pictures.length-1
+    }
+    if (object == 'slider__arrow slider__arrow_next'){ 
+        pictIndex = pictIndex < pictures.length-1 ? pictIndex += 1 : pictIndex = 0
+    }   
+    pictures[pictIndex].className='slider__item slider__item_active' 
 }
-    if (object == 'slider__arrow slider__arrow_next'){ console.log('next')}
-    pictures[0].children[pictIndex].className.includes( 'hole_has-mole' )
-  
+
+function callback(item) {
+    if (item.className == 'slider__item slider__item_active') {
+        return true
+    }
 
 }
 
+//.includes
 clickFound()
